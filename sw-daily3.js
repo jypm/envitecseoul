4761205111f9329aab453ccb8d61280d7f30fb1a
jypm <87839718+jypm@users.noreply.github.com>
2026-08-30 21:02:32 +0900

    Add files via upload

--- sw-daily3.js
+++ sw-daily3.js
@@ -1,25 +1,26 @@
-const CACHE='sugeo-daily3-v8';
+const CACHE='sugeo-daily3-v9';
 const FILES=[
   '/envitecseoul/daily3.html',
   '/envitecseoul/manifest_daily3.json',
   '/envitecseoul/icon_daily3.png',
   '/envitecseoul/icon_daily3-180.png',
   '/envitecseoul/icon_daily3-192.png',
-  '/envitecseoul/icon_daily3-512.png',
-  './daily3.html',
-  './manifest_daily3.json',
-  './icon_daily3.png',
-  './icon_daily3-180.png',
-  './icon_daily3-192.png',
-  './icon_daily3-512.png'
+  '/envitecseoul/icon_daily3-512.png'
 ];
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES).catch(err=>console.log('cache fail',err))));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
   self.skipWaiting();
 });
 self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  // Only handle same-origin
+  if(!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    // update cache
+    const copy=res.clone();
+    caches.open(CACHE).then(c=>c.put(e.request, copy));
+    return res;
+  }).catch(()=>caches.match('/envitecseoul/daily3.html'))));
 });
