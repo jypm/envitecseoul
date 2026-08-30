d52e81bf0645d890993c954f8c1f28fd4c65a713
jypm <87839718+jypm@users.noreply.github.com>
2026-08-30 20:49:58 +0900

    Delete Sw-Daily3.js

--- Sw-Daily3.js
+++
@@ -1,4 +0,0 @@
-const CACHE='sugeo-daily3-v2';
-const FILES=['./daily3.html','./manifest_daily3.json','./icon_daily3.png','./icon_daily3-192.png','./icon_daily3-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
